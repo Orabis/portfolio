f2d29f186d3954de871d4e32372b5b23c48f658a
Orabis <email.orabis@gmail.com>
2024-09-01 01:46:17 +0200

    added moving img, responsive img, css dark mage things, and a "Bienvenue" !

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import './style.css'
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        const welcome = document.getElementById('welcome') as HTMLElement;
+        const mainContent = document.getElementById('contents') as HTMLElement;
 
-document.addEventListener("DOMContentLoaded", () => {
-    const element = document.querySelector(".fadeOut");
-    if (element){
-        element.addEventListener("animationend", () =>{
-            element.remove();
-        })
-    }
-})
+        if (welcome && mainContent) {
+            welcome.classList.add('fadeOut');
 
+            setTimeout(() => {
+                welcome.style.display = 'none';
+                mainContent.style.display = 'block';
+            }, 2000);
+        }
+    }, 1000);
+});
