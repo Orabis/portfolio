3d5704f696af3308ad4f16e7fc80e5e74009c042
Leo <cdf.leo.merkel@gmail.com>
2024-08-31 17:33:18 +0200

    add fade to welcome, menu added

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import './style.css'
+
+document.addEventListener("DOMContentLoaded", () => {
+    const element = document.querySelector(".fadeOut");
+    if (element){
+        element.addEventListener("animationend", () =>{
+            element.remove();
+        })
+    }
+})
+
