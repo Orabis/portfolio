51d022c892aa27bab9fe29a7baf3b7e2b7abb071
Orabis <email.orabis@gmail.com>
2024-09-01 03:07:34 +0200

    J'ai cook, animation smooth, accueil fonctionnel, visage centré avec une flexbox. Bref 1ere partie faites. a peu pres.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import './style.css'
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
-        const welcome = document.getElementById('welcome') as HTMLElement;
+        const welcome = document.querySelector('.welcome') as HTMLElement;
         const mainContent = document.getElementById('contents') as HTMLElement;
+        const body = document.body;
 
         if (welcome && mainContent) {
             welcome.classList.add('fadeOut');
+            mainContent.classList.remove("fadeIn");
+            mainContent.classList.add('contents');
 
             setTimeout(() => {
                 welcome.style.display = 'none';
-                mainContent.style.display = 'block';
+                body.classList.remove("blockInteract")
             }, 2000);
         }
     }, 1000);
