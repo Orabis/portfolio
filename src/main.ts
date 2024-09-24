f6b52d9fe9bfa2f74a714bb393a7b10b3ba1b7fd
Leo <cdf.leo.merkel@gmail.com>
2024-09-24 22:31:43 +0200

    rework all skills, need css adjustement

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import './style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
-        const welcome = document.querySelector('.welcome') as HTMLElement;
-        const mainContent = document.getElementById('contents') as HTMLElement;
+        const welcome = document.querySelector('.welcome') as HTMLElement | null;
+        const mainContent = document.getElementById('contents') as HTMLElement | null;
         const body = document.body;
 
         if (welcome && mainContent) {
@@ -17,11 +17,33 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 2000);
         }
     }, 1000);
-});
-window.onload = () => {
+
+        const mainButton = document.getElementById("main-button") as HTMLButtonElement | null;
+        const secondButton = document.getElementById("second-button") as HTMLButtonElement | null;
+        const thirdButton = document.getElementById("third-button") as HTMLButtonElement | null;
+
+        const mainSkills = document.getElementById("main-skills-list") as HTMLDivElement | null;
+        const secondSkills = document.getElementById("second-skills-list") as HTMLDivElement | null;
+        const thirdSkills = document.getElementById("third-skills-list") as HTMLDivElement | null;
+
+        if (mainButton && secondButton && thirdButton && mainSkills && secondSkills && thirdSkills) {
+            function showSkills(selected: HTMLElement, toHide1: HTMLElement, toHide2: HTMLElement) {
+                selected.style.display = 'grid';
+                toHide1.style.display = 'none';
+                toHide2.style.display = 'none';
+            }
+            mainButton.addEventListener('click', () => showSkills(mainSkills, secondSkills, thirdSkills));
+            secondButton.addEventListener('click', () => showSkills(secondSkills, mainSkills, thirdSkills));
+            thirdButton.addEventListener('click', () => showSkills(thirdSkills, mainSkills, secondSkills));
+        } else {
+            console.error("Un ou plusieurs éléments du DOM ne sont pas chargés correctement.");
+        }
+    });
+
+    window.onload = () => {
     window.scroll(0, 0);
     setTimeout(() => {
-        const timeline = document.getElementById('timeline') as HTMLElement;
+        const timeline = document.getElementById('timeline') as HTMLElement | null;
         const events = document.querySelectorAll('.event') as NodeListOf<HTMLElement>;
         const progressBars = document.querySelectorAll('.progressbar') as NodeListOf<HTMLElement>;
 
@@ -34,6 +56,7 @@ window.onload = () => {
                 rect.right <= (window.innerWidth || document.documentElement.clientWidth)
             );
         };
+
         const handleScroll = () => {
             if (timeline && isInViewport(timeline)) {
                 timeline.style.width = '100%';
@@ -42,6 +65,7 @@ window.onload = () => {
                     events.forEach(event => event.style.opacity = '1');
                 }, 900);
             }
+
             progressBars.forEach((progressBar) => {
                 const value = progressBar.getAttribute('data-value');
                 if (value && isInViewport(progressBar)) {
@@ -49,6 +73,7 @@ window.onload = () => {
                 }
             });
         };
+
         window.addEventListener('scroll', handleScroll);
     }, 3000);
 };
